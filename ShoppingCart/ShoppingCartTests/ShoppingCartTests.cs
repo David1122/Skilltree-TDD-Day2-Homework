8ae6d4fc16798067ae791e40a37dfd4c615a20ec
WeiDu <windwind112210@gmail.com>
2015-10-29 09:33:45 +0800

    案例二：紅燈

--- ShoppingCart/ShoppingCartTests/ShoppingCartTests.cs
+++ ShoppingCart/ShoppingCartTests/ShoppingCartTests.cs
@@ -23,5 +23,22 @@ namespace ShoppingCartTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void 第一集買了一本_第二集也買了一本_價格應為100乘以2乘以0點95等於190()
+        {
+            var bookList = new List<Book>();
+            bookList.Add(new Book(Book.Episode.Episode1, 1));
+            bookList.Add(new Book(Book.Episode.Episode2, 1));
+            bookList.Add(new Book(Book.Episode.Episode3, 0));
+            bookList.Add(new Book(Book.Episode.Episode4, 0));
+            bookList.Add(new Book(Book.Episode.Episode5, 0));
+            var expected = 190;
+            var target = new ShoppingCart(bookList);
+
+            var actual = target.CalculateTotal();
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
