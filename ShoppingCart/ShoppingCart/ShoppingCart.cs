9fb00ca8efa015b103d7b511c3635aef72711278
WeiDu <windwind112210@gmail.com>
2015-10-29 09:20:28 +0800

    案例一：紅燈

---
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Homework
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(List<Book> shoppingCart)
+        {
+            
+        }
+
+        public object CalculateTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Book
+    {
+        public Episode episode { get; set; }
+
+        public int qty { get; set; }
+
+        public readonly int unitPrice = 100;
+
+        public enum Episode
+        {
+            Episode1,
+            Episode2,
+            Episode3,
+            Episode4,
+            Episode5
+        }
+
+        public Book(Episode episode, int qty)
+        {
+            this.episode = episode;
+            this.qty = qty;
+        } 
+    }
+}
---
+++ ShoppingCart/ShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2Homework;
+
+namespace ShoppingCartTests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void 第一集買了一本_其他都沒買_價格應為100乘以1等於100元()
+        {
+            var bookList = new List<Book>();
+            bookList.Add(new Book(Book.Episode.Episode1, 1));
+            bookList.Add(new Book(Book.Episode.Episode2, 0));
+            bookList.Add(new Book(Book.Episode.Episode3, 0));
+            bookList.Add(new Book(Book.Episode.Episode4, 0));
+            bookList.Add(new Book(Book.Episode.Episode5, 0));
+            var expected = 100;
+            var target = new ShoppingCart(bookList);
+
+            var actual = target.CalculateTotal();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
